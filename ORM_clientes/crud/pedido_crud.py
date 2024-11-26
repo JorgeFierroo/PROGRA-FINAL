2f1275ef29650fdd7b17efcc56af33da3f2c6e7b
OscarCCGG <ocontreras2024@alu.uct.cl>
2024-11-26 15:55:13 -0300

    Update pedido_crud.py

---
+++ ORM_clientes/crud/pedido_crud.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import Session
+class PedidoCRUD:
+
+    @staticmethod
+    def crear_pedido(db:Session):
+        pass
+
+    @staticmethod
+    def leer_pedido(db:Session):
+        pass
+
+    @staticmethod
+    def actualizar_pedido(db:Session):
+        pass
+
+    @staticmethod
+    def eliminar_pedido(db:Session):
+        pass
