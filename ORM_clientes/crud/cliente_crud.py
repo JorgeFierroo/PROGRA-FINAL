58dc09130f43ecf3105a37f214360e2bcc6a90f7
OscarCCGG <ocontreras2024@alu.uct.cl>
2024-11-26 15:55:03 -0300

    Update cliente_crud.py

---
+++ ORM_clientes/crud/cliente_crud.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import Session
+class ClienteCRUD:
+
+    @staticmethod
+    def crear_cliente(db:Session):
+        pass
+
+    @staticmethod
+    def leer_cliente(db:Session):
+        pass
+
+    @staticmethod
+    def actualizar_cliente(db:Session):
+        pass
+
+    @staticmethod
+    def eliminar_cliente(db:Session):
+        pass
